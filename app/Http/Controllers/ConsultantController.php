d960ec3a9f7bf07c0f1b1898fb53557a2066c5e3
Kesavi-Kanesalingam <52402102+Kesavi-Kanesalingam@users.noreply.github.com>
2019-09-21 07:35:27 +0530

    Consultant controller completed

--- app/Http/Controllers/ConsultantController.php
+++ app/Http/Controllers/ConsultantController.php
@@ -14,12 +14,15 @@ class ConsultantController extends Controller
      */
     public function index()
     {
-        //
+        $consultants=Consultants::all();
+
+        return View::make('consultants.index')
+            ->with('consultants', $consultants);
     }
 
     /**
      * Show the form for creating a new resource.
-     *
+     
      * @return \Illuminate\Http\Response
      */
     public function create()
@@ -35,7 +38,33 @@ class ConsultantController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try{
+            $response = array(
+                'status'=>"Failed",
+                'msg'=>'',
+                'is_success'=>false,
+                'data'=>''
+            );
+            $consultant = new consultant();
+
+                $consultant->id = $request->id;
+                $consultant->doctor_id = $request->doctor_id;
+                $consultant->specialist = $request->specialist;
+
+                $consultant->save();
+            $response['status'] = "Success";
+            $response['msg'] = "Add consultant";
+            $response['is_success'] = true;
+            $response['data'] =$consultant;
+            return response()->json($response);
+        }
+        catch(Exception $e)
+        {
+            $response['status'] = "Failed";
+            $response['msg'] = $e->getMessage();
+            $response['is_success'] = false;
+            return response()->json($response); 
+        }
     }
 
     /**
@@ -44,9 +73,29 @@ class ConsultantController extends Controller
      * @param  \App\Consultant  $consultant
      * @return \Illuminate\Http\Response
      */
-    public function show(Consultant $consultant)
+    public function show($id)
     {
-        //
+        try{
+            $response = array(
+                'status'=>"Failed",
+                'msg'=>'',
+                'is_success'=>false,
+                'data'=>''
+            );
+            $consultant = Consultant::find($id);
+            $response['status'] = "Success";
+            $response['msg'] = "Get one consultant";
+            $response['is_success'] = true;
+            $response['data'] = array('consultant-'=>$consultant);
+            return response()->json($response);
+        }
+        catch(Exception $e)
+        {
+            $response['status'] = "Failed";
+            $response['msg'] = $e->getMessage();
+            $response['is_success'] = false;
+            return response()->json($response); 
+        }
     }
 
     /**
@@ -67,9 +116,35 @@ class ConsultantController extends Controller
      * @param  \App\Consultant  $consultant
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Consultant $consultant)
+    public function update(Request $request, $id)
     {
-        //
+        try{
+            $response = array(
+                'status'=>"Failed",
+                'msg'=>'',
+                'is_success'=>false,
+                'data'=>''
+            );
+            $consultant = Consultant::find($id);
+
+            $consultant->id = $request->id;
+            $consultant->doctor_id = $request->doctor_id;
+            $consultant->specialist = $request->specialist;
+
+            $consultant->save();
+            $response['status'] = "Success";
+            $response['msg'] = "Update consultant";
+            $response['is_success'] = true;
+            $response['data'] =$consultant;
+            return response()->json($response);
+        }
+        catch(Exception $e)
+        {
+            $response['status'] = "Failed";
+            $response['msg'] = $e->getMessage();
+            $response['is_success'] = false;
+            return response()->json($response); 
+        }
     }
 
     /**
@@ -78,8 +153,28 @@ class ConsultantController extends Controller
      * @param  \App\Consultant  $consultant
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Consultant $consultant)
+    public function destroy($id)
     {
-        //
+        try{
+            $response = array(
+                'status'=>"Failed",
+                'msg'=>'',
+                'is_success'=>false,
+                'data'=>''
+            );
+            $consultant = Consultant::find($id);
+            $consultant->delete();
+            $response['status'] = "Success";
+            $response['msg'] = "Delete consultant";
+            $response['is_success'] = true;
+            return response()->json($response);
+        }
+        catch(Exception $e)
+        {
+            $response['status'] = "Failed";
+            $response['msg'] = $e->getMessage();
+            $response['is_success'] = false;
+            return response()->json($response); 
+        }
     }
 }
